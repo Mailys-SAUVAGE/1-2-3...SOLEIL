119a5dd448646d404d12e70366dce11ed6cf1734
Mailys-SAUVAGE <sauvagemailys@gmail.com>
2025-05-15 10:44:52 +0200

    Create Ultrason.ino

---
+++ Ultrason.ino
@@ -0,0 +1,11 @@
+float lireDistanceUltrason() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  long duration = pulseIn(ECHO_PIN, HIGH);
+  float distance = duration * 0.034 / 2;
+  return distance;
+}
