0f4959c0f440c2db976a5c97f5f51d7e15647e86
Mailys-SAUVAGE <sauvagemailys@gmail.com>
2025-05-25 20:51:36 +0200

    Fin Buzzer.ino

--- Buzzer.ino
+++ Buzzer.ino
@@ -1,9 +1,19 @@
-// Son LED allumée
 void sonAllumageLED() {
   tone(A4, 1000, 80);
 }
 
-// Son d'échec
 void sonPerdu() {
   tone(A4, 200, 500);
+}
+
+void sonVictoire() {
+  int notes[] = { 523, 659, 784, 988 };  // Do - Mi - Sol - Si aigu
+  int duration = 150;
+
+  for (int i = 0; i < 4; i++) {
+    tone(BUZZER_PIN, notes[i], duration);
+    delay(duration + 50);  // petit espace entre les notes
+  }
+
+  noTone(BUZZER_PIN);
 }
