260aaf1760f53b3d2fe4f2ed694d93ff0b7087f7
Mailys-SAUVAGE <sauvagemailys@gmail.com>
2025-05-25 21:31:03 +0200

    minor fix Bouton.ino

--- Bouton.ino
+++ Bouton.ino
@@ -1,22 +1,23 @@
-extern bool jeuEnCours; // variable de loop.ino
+  extern bool jeuEnCours; // variable de loop.ino
 
-bool gererBouton() {
-  int etat = digitalRead(BUTTON_PIN);
+  bool gererBouton() {
+    int etat = digitalRead(BUTTON_PIN);
 
-  if (etat == LOW) {
-    return true;
-  } else {
+    if (etat == LOW) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // Vérifie bouton, interrompt le jeu si appuyé
+  bool verifierFin() {
+    if (gererBouton()){
+        sonVictoire();
+        Serial.println("VICTOIRE !!!");
+        allumerTout(CRGB(255, 215, 0));
+        jeuEnCours = false;
+        return true;
+    }
     return false;
   }
-}
-
-// Vérifie bouton, interrompt le jeu si appuyé
-bool verifierFin() {
-  if (gererBouton()){
-      sonVictoire();
-      allumerTout(CRGB(255, 215, 0));
-      jeuEnCours = false;
-      return true;
-  }
-  return false;
-}
