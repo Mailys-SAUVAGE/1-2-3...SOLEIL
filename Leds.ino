4e7b2c22cd408fc75dca18182165d88f0d8e867d
Mailys-SAUVAGE <sauvagemailys@gmail.com>
2025-05-25 20:51:18 +0200

    Fin Leds.ino

--- Leds.ino
+++ Leds.ino
@@ -1,13 +1,38 @@
+extern bool jeuEnCours; // variable de loop.ino
+
 void allumerLeds(CRGB couleur, int delai) {
   eteindreLeds();
+
   for (int i = 1; i < NUM_LEDS; i++) {
     leds[i] = couleur;
+
+    // Afficher toutes les 5 LEDs ou à la fin
     if ((i - 1) % 5 == 4 || i == NUM_LEDS - 1) {
       FastLED.show();
       sonAllumageLED();
-      delay(delai);
+
+      // ⏳ Attente divisée en petits pas pour pouvoir interrompre
+      int pas = 10;  // 10 ms d'attente
+      int attente = 0;
+      while (attente < delai) {
+        if (verifierFin()){
+          return;  // 👈 interrompt l'allumage proprement
+        }
+        delay(pas);
+        attente += pas;
+      }
     }
   }
+}
+
+
+void allumerTout(CRGB couleur) {
+  for (int i = 1; i < NUM_LEDS; i++) {
+    leds[i] = couleur;
+  }
+  FastLED.show();
+  sonPerdu();
+  delay(1000);
 }
 
 void eteindreLeds() {
